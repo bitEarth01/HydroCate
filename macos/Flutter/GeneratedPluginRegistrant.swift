738a252c8821b59605b47a980c668276d15fb838
Manoj <manojparasuram.sadhanala@gmail.com>
2023-10-08 07:33:00 +0530

    updated packages

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import flutter_curl
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FlutterCurlPlugin.register(with: registry.registrar(forPlugin: "FlutterCurlPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
